f5edf59ae31f60954580f95e6d0f230757999927
SAHIL-8398 <129992835+SAHIL-8398@users.noreply.github.com>
2026-08-17 00:30:23 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,10 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+V6import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.shoppos.pro',
   appName: 'ShopPOS Pro',
   webDir: 'dist',
+  backgroundColor: '#00000000',
   server: {
     androidScheme: 'https',
     cleartext: true
